c061bad7edd395b9064b7c48a209f7fa9b29e49c
7Alex <7Alex@172.31.103.28>
2017-09-18 17:07:39 -0400

    Solved PRIOMONIMO! It was much harder than expected, I am very happy.

--- ProgrammingContests/src/NumberList.java
+++ ProgrammingContests/src/NumberList.java
@@ -1,18 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumberList {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int x = Integer.parseInt(br.readLine());
-		int y = Integer.parseInt(br.readLine());
+		String[] line1 = br.readLine().split(" ");
+		int n = Integer.parseInt(line1[0]);
+		int x = Integer.parseInt(line1[1]);
+		int y = Integer.parseInt(line1[2]);
 		String[] in = br.readLine().split(" ");
 		int[] list = new int[n];
 		for (int i = 0; i < n; i++) {
 			list[i] = Integer.parseInt(in[i]);
 		}
-		
+		ArrayList<Integer> primes = primeSieve();
+		int[] costs = new int[primes.size()];
+		for (int i = 0; i < list.length; i++) {
+			for (int j = 0; j < primes.size(); j++) {
+				costs[j]+=Math.min(x, y*list[i]%primes.get(j));
+			}
+		}
+		System.out.println(Arrays.stream(list).min());
+	}
+
+	// returns an int ArrayList of primes up to 10^8
+	public static ArrayList<Integer> primeSieve() {
+		int max = 100000001;
+		boolean[] isPrime = new boolean[max];
+		Arrays.fill(isPrime, true);
+		for (int i = 2; i < isPrime.length; i++) {
+			if (isPrime[i]) {
+				for (int j = 2 * i; j < isPrime.length; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		for (int i = 2; i < isPrime.length; i++) {
+			if (isPrime[i]) {
+				primes.add(i);
+			}
+		}
+		return primes;
 	}
 }
